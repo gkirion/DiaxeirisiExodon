c9fd1adf8a68a35f53008fdb52290d488870d1dd
unknown <gkirion@gmail.com>
2020-06-08 19:12:54 +0300

    added search for last 10 expenses and by date

--- src/main/java/com/george/app/endpoint/ExpenseRest.java
+++ src/main/java/com/george/app/endpoint/ExpenseRest.java
@@ -5,15 +5,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.*;
 
 import com.george.app.entity.Category;
 import com.george.app.entity.Expense;
@@ -51,7 +44,17 @@ public class ExpenseRest {
 	public List<Expense> getAll() {
 		return expenseRepository.findAll();
 	}
-	
+
+	@GetMapping("/last10")
+	public List<Expense> findFirst10OrderByDateDesc() {
+		return expenseRepository.findFirst10ByOrderByDateDesc();
+	}
+
+	@GetMapping("/byDate")
+	public List<Expense> findByDateBetweenOrderByDateDesc(@RequestParam("start") @DateTimeFormat(pattern = "dd/MM/yyyy HH:mm:ss") Date start, @RequestParam("end") @DateTimeFormat(pattern = "dd/MM/yyyy HH:mm:ss") Date end) {
+		return expenseRepository.findByDateBetweenOrderByDateDesc(start, end);
+	}
+
 	@GetMapping("/{id}")
 	public Expense get(@PathVariable Integer id) throws ExpenseNotFoundException {
 		Optional<Expense> expense = expenseRepository.findById(id);
--- src/main/java/com/george/app/repository/ExpenseRepository.java
+++ src/main/java/com/george/app/repository/ExpenseRepository.java
@@ -1,5 +1,6 @@
 package com.george.app.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,5 +10,9 @@ import com.george.app.entity.Expense;
 public interface ExpenseRepository extends JpaRepository<Expense, Integer> {
 	
 	public List<Expense> findByCategoryId(Integer categoryId);
+
+	public List<Expense> findFirst10ByOrderByDateDesc();
+
+	public List<Expense> findByDateBetweenOrderByDateDesc(Date start, Date end);
 
 }
